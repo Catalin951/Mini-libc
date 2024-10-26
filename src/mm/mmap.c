b5a9c17761910cfbfbafc199d513bd8709ed96c8
Manole Catalin-Gabriel <catalin.manole1211@stud.acs.upb.ro>
2024-10-26 21:59:28 +0000

    implementare mmap, mremap si munmap

--- src/mm/mmap.c
+++ src/mm/mmap.c
@@ -1,23 +1,55 @@
 // SPDX-License-Identifier: BSD-3-Clause
-
+#include <sys/stat.h>
 #include <sys/mman.h>
 #include <errno.h>
 #include <internal/syscall.h>
-
+#include <unistd.h>
 void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
 {
-	/* TODO: Implement mmap(). */
-	return MAP_FAILED;
+	struct stat st;
+	// not map_anonymous
+	if(!(flags & MAP_ANONYMOUS)) {
+		// fd must not be -1
+		if(fd == -1) {
+			errno = EBADF;
+			return MAP_FAILED;
+		}
+		// check if fd is valid
+		// fstat modifies errno
+		fstat(fd, &st);
+		if (errno == EBADF)
+			return MAP_FAILED;
+	}
+	// check if mapping is anonymous and
+	// none of private and shared flags are on
+	if((flags & MAP_ANONYMOUS) && !(flags & MAP_PRIVATE) && !(flags & MAP_SHARED)) {
+		errno = EINVAL;
+		return MAP_FAILED;
+	}
+
+	long result = syscall(9, addr, length, prot, flags, fd, offset);
+	// pointer to memory
+	return (void *) result;
 }
 
 void *mremap(void *old_address, size_t old_size, size_t new_size, int flags)
 {
-	/* TODO: Implement mremap(). */
-	return MAP_FAILED;
+	if (!old_address) {
+		errno = EINVAL;
+		return MAP_FAILED;
+	}
+	void *result = (void *) syscall(25, old_address, old_size, new_size, flags);
+	if (result == MAP_FAILED)
+		return result;
+	return result;
 }
 
 int munmap(void *addr, size_t length)
 {
-	/* TODO: Implement munmap(). */
-	return -1;
+	long result = syscall(11, addr, length);
+	if (result < 0) {
+		errno = -result;
+		return -1;
+	}
+	return result;
 }
